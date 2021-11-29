9cb2ba8a32caf10efa059a5fe74101da5b585088
FeLaar <57942217+FeLaar@users.noreply.github.com>
2021-11-29 13:21:28 +0100

    Create poortTwee.ino

---
+++ poortTwee.ino
@@ -0,0 +1,14 @@
+/*
+var KnikkerPoort = new KnikkerPoort(//pin iets,
+  // gradenopen
+  // graden dicht inbullen); */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
