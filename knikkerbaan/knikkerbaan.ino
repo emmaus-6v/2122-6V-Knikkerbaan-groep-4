8adb22874b5739fc241d7771bd3fdb3a5bba641b
FrederiqueK <57898671+FrederiqueK@users.noreply.github.com>
2022-02-16 09:57:53 +0100

    Update knikkerbaan.ino

--- knikkerbaan/knikkerbaan.ino
+++ knikkerbaan/knikkerbaan.ino
@@ -6,8 +6,8 @@ const int BOVEN_POORT_PIN = 7;    // pin van servo die bovenste poort regelt
 const int LEEGLOOP_TIJD = 5000;   // tijd die knikkers nodig hebben om na het
                                   // sluiten van de poort langs alle sensors te komen
 
-char WIFI_NETWERK[] = "Bougainvillea";
-char WIFI_WACHTWOORD[] = "aikzmidwmznzgzndkmebvz";
-char SERVER_DOMEINNAAM[] = "3000-emmaus6v-21226vknikkerba-ko0llhn63hk.ws-eu31.gitpod.io";    // domeinnaam van gitpod-server, zoals 3000-lavendel-bla-bla.gitpod.io
+char WIFI_NETWERK[] = "";
+char WIFI_WACHTWOORD[] = "";
+char SERVER_DOMEINNAAM[] = "";    // domeinnaam van gitpod-server, zoals 3000-lavendel-bla-bla.gitpod.io
 
 const int TELLER_A_PIN = 5;       // pin waarop IR-sensor voor Teller A is aangesloten
