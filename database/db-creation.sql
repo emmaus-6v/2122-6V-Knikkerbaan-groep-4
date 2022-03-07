85993e6ab4eeccd74c3426281af1f692a464c119
FrederiqueK <57898671+FrederiqueK@users.noreply.github.com>
2022-03-07 23:20:52 +0100

    poortPositie

--- database/db-creation.sql
+++ database/db-creation.sql
@@ -27,5 +27,5 @@ CREATE TABLE instellingen (
 /* Indien je standaard wat gegevens in de database wilt,
    voeg hieronder dan INSERT regels to */
 INSERT INTO runs (stamp) VALUES (CURRENT_TIMESTAMP);
-INSERT INTO sensorData (run, stamp, aantalKnikkers, buttonState, poortPositie) VALUES (1, CURRENT_TIMESTAMP, 21, 3, 5);
+INSERT INTO sensorData (run, stamp, aantalKnikkers, buttonState, poortPositie) VALUES (1, CURRENT_TIMESTAMP, 21, 1, "links");
 INSERT INTO instellingen (run, stamp, wachttijdPoort) VALUES (1, CURRENT_TIMESTAMP, 15);
