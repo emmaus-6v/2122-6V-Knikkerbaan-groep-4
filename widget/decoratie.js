a513b54ee2b04234596a224384a0169c9a385157
naomilubeek <57898296+naomilubeek@users.noreply.github.com>
2022-03-08 23:33:04 +0100

    Update decoratie.js

--- widget/decoratie.js
+++ widget/decoratie.js
@@ -1,7 +1,9 @@
-class Decoratie { // dit is de class met de code voor de decoratie bloemen in de widget
+/* Class voor de opmaak vand de widget. Wordt gebruikt voor het tekenen van de groene 'planten'. */
+
+class Decoratie { 
   
-  constructor(newX, newY) {
-    this.x = newX;
+  constructor(newX, newY) { // de grootte van de 'bloemen' zijn hetzelfde, maar je kunt met het object
+    this.x = newX;          // telkens een nieuwe toevoegen op een coördinaat
     this.y = newY;
   }
 
@@ -9,17 +11,18 @@ class Decoratie { // dit is de class met de code voor de decoratie bloemen in de
 
   }
   
-  display() {
+  /* dit tekent de 'bloem' */
+  display() { 
     fill(32, 89, 47, 127);
     stroke(127,63,120);
-    push();
+    push(); 
     translate(this.x, this.y);
     noStroke();
     for (let i = 0; i < 10; i ++) {
       ellipse(0, 1, 12, 60);
       rotate(PI/7);
     }
-    pop();
+    pop(); // dmv push en pop kun je meerdere 'bloemen' tekenen
   }
 
 
