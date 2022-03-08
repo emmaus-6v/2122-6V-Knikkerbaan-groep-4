66a4b0fabd95064bbac77abc1de261d29bfceb07
Luuk684 <75067168+Luuk684@users.noreply.github.com>
2022-03-08 21:50:06 +0100

    commentaar

--- database/db-creation.sql
+++ database/db-creation.sql
@@ -9,9 +9,9 @@ CREATE TABLE sensorData (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   run INTEGER NOT NULL,
   stamp DATETIME NOT NULL,
-  aantalKnikkers INTEGER,
-  buttonState INTEGER,
-  poortPositie INTEGER
+  aantalKnikkers INTEGER, // Geeft het aantal knikkers aan 
+  buttonState INTEGER, // Geeft de status van de knop aan (0 = LOW, 1 = HIGH)
+  poortPositie INTEGER /* Geeft de richting aan van de poort (0 = de knikkers gaan naar links, 1 = de knikkers gaan naar rechts) */
 );
 
 CREATE TABLE instellingen (
@@ -27,5 +27,5 @@ CREATE TABLE instellingen (
 /* Indien je standaard wat gegevens in de database wilt,
    voeg hieronder dan INSERT regels to */
 INSERT INTO runs (stamp) VALUES (CURRENT_TIMESTAMP);
-INSERT INTO sensorData (run, stamp, aantalKnikkers, buttonState, poortPositie) VALUES (1, CURRENT_TIMESTAMP, 21, 1, 1);
+INSERT INTO sensorData (run, stamp, aantalKnikkers, buttonState, poortPositie) VALUES (1, CURRENT_TIMESTAMP, 21, 0, 0);
 INSERT INTO instellingen (run, stamp, wachttijdPoort) VALUES (1, CURRENT_TIMESTAMP, 15);
