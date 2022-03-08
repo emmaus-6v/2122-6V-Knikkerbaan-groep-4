8157250978a855b68deb404df8f044fdf0654b99
naomilubeek <57898296+naomilubeek@users.noreply.github.com>
2022-03-08 23:25:59 +0100

    Update db-creation.sql

--- database/db-creation.sql
+++ database/db-creation.sql
@@ -1,24 +1,27 @@
 /* Beschrijf de tabellen die je nodig hebt*/
 
+/* Tabel voor de runs*/ 
 CREATE TABLE runs (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   stamp DATETIME NOT NULL
 );
 
+/* Tabel met de data van de knikkerbaan*/
 CREATE TABLE sensorData (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   run INTEGER NOT NULL,
   stamp DATETIME NOT NULL,
-  aantalKnikkers INTEGER, /* Geeft het aantal knikkers aan */
-  buttonState INTEGER, /* Geeft de status van de knop aan (0 = LOW, 1 = HIGH) */
-  poortPositie INTEGER /* Geeft de richting aan van de poort (0 = de knikkers gaan naar links, 1 = de knikkers gaan naar rechts) */
+  aantalKnikkers INTEGER,                 /* Geeft het aantal knikkers aan */
+  buttonState INTEGER,                    /* Geeft de status van de knop aan (0 = LOW, 1 = HIGH) */
+  poortPositie INTEGER                    /* Geeft de richting aan van de poort (0 = de knikkers gaan naar links, 1 = de knikkers gaan naar rechts) */
 );
 
+/* Tabel met de instellingen */
 CREATE TABLE instellingen (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   run INTEGER NOT NULL,
   stamp DATETIME NOT NULL,
-  wachttijdPoort INTEGER NOT NULL
+  wachttijdPoort INTEGER NOT NULL         /* Hoe lang de poort moet wachten, dit kan niet 0 zijn */
 );
 
 
